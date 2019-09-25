daef5d822b2cb5571bc96b22d8c70f9bed3d1a89
Tom <tomleewu@gmail.com>
2019-09-25 13:12:13 -0400

    Added Linux goodies

---
+++ .screenlayout/1440p_monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --off --output DP1-1 --off --output DP1-2-8 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1-3 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off
