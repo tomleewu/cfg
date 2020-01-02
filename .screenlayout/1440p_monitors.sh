1e2caa95c9a776392d4d9a6c0f15db67d7ddf41e
Tom Wu <tomleewu@gmail.com>
2020-01-02 15:07:58 -0500

    Latest changes

--- .screenlayout/1440p_monitors.sh
+++ .screenlayout/1440p_monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP1-1 --off --output DP1-2-8 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1-3 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-1-1 --off --output DP-1-2-8 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-2-1 --off --output DP-1-2 --off --output DP-2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal
