c120a86ad35d007bb1983aedb6627b3ae724181f
Tom <tomleewu@gmail.com>
2022-09-11 12:38:48 -0400

    enable lualine lol

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -1,8 +1,8 @@
 local remap = require("tomleewu.keymap")
 local nmap = remap.nmap
 -- General settings
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
 
 -- General keymaps
 nmap("<C-c>", "<cmd>GoTestFunc!<CR>")
--- .config/nvim/lua/tomleewu/lsp/installer.lua
+++ .config/nvim/lua/tomleewu/lsp/installer.lua
@@ -1,29 +1,29 @@
 local M = {}
 
 function M.setup(servers, server_options)
-  local lspconfig = require "lspconfig"
+	local lspconfig = require "lspconfig"
 
-  require("mason").setup()
+	require("mason").setup()
 
-  require("mason-lspconfig").setup({
-    ensure_installed = vim.tbl_keys(servers),
-    automatic_installation = false
-  })
+	require("mason-lspconfig").setup({
+		ensure_installed = vim.tbl_keys(servers),
+		automatic_installation = false
+	})
 
-  require("mason-lspconfig").setup_handlers {
-    function(server_name)
-      local opts = vim.tbl_deep_extend("force", server_options, servers[server_name] or {})
-      lspconfig[server_name].setup(opts)
-    end,
-    -- ["sumneko_lua"] = function()
-    --   local opts = vim.tbl_deep_extend("force", server_options, servers["sumneko_lua"] or {})
-    --   -- lspconfig.sumneko_lua.setup(require("lua-dev").setup { lspconfig = opts })
-    -- end,
-    -- ["gopls"] = function()
-    --   local opts = vim.tbl_deep_extend("force", server_options, servers["gopls"] or {})
-    --   lspconfig["gopls"].setup(opts)
-    -- end,
-  }
+	require("mason-lspconfig").setup_handlers {
+		function(server_name)
+			local opts = vim.tbl_deep_extend("force", server_options, servers[server_name] or {})
+			lspconfig[server_name].setup(opts)
+		end,
+		-- ["sumneko_lua"] = function()
+		--   local opts = vim.tbl_deep_extend("force", server_options, servers["sumneko_lua"] or {})
+		--   -- lspconfig.sumneko_lua.setup(require("lua-dev").setup { lspconfig = opts })
+		-- end,
+		-- ["gopls"] = function()
+		--   local opts = vim.tbl_deep_extend("force", server_options, servers["gopls"] or {})
+		--   lspconfig["gopls"].setup(opts)
+		-- end,
+	}
 
 end
 
