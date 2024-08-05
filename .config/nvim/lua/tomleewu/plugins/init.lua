37ac9effd41d91dc8d9b223d141a510270473501
Tom Wu <tomleewu@users.noreply.github.com>
2024-08-04 20:02:36 -0600

    Replace leap w/ flash

--- .config/nvim/lua/tomleewu/plugins/init.lua
+++ .config/nvim/lua/tomleewu/plugins/init.lua
@@ -17,16 +17,7 @@ return {
     'alexghergh/nvim-tmux-navigation',
     -- automated indention
     'tpope/vim-sleuth',
-    {
-        'ggandor/leap.nvim',
-        dependencies = {
-            { 'ggandor/flit.nvim', opts = { labeled_modes = "nv", multiline = false } }
-        },
-        config = function()
-            require 'leap'.add_default_mappings()
-        end,
-        event = "VeryLazy"
-    },
+
     -- colorscheme
     {
         'tomleewu/vim-paper',
@@ -152,70 +143,4 @@ return {
         ft = { "markdown" },
         build = function() vim.fn["mkdp#util#install"]() end,
     },
-    {
-        "rachartier/tiny-inline-diagnostic.nvim",
-        event = "VeryLazy",
-        config = function()
-            -- Default configuration
-            require("tiny-inline-diagnostic").setup({
-                signs = {
-                    left = "",
-                    right = "",
-                    diag = "●",
-                    arrow = "    ",
-                    up_arrow = "    ",
-                    vertical = " │",
-                    vertical_end = " └",
-                },
-                hi = {
-                    error = "DiagnosticError",
-                    warn = "DiagnosticWarn",
-                    info = "DiagnosticInfo",
-                    hint = "DiagnosticHint",
-                    arrow = "NonText",
-                    background = "CursorLine", -- Can be a highlight or a hexadecimal color (#RRGGBB)
-                    mixing_color = "#EBE5CC",  -- Can be None or a hexadecimal color (#RRGGBB). Used to blend the background color with the diagnostic background color with another color.
-                },
-                blend = {
-                    factor = 0.27,
-                },
-                options = {
-                    -- Show the source of the diagnostic.
-                    show_source = false,
-
-                    -- Throttle the update of the diagnostic when moving cursor, in milliseconds.
-                    -- You can increase it if you have performance issues.
-                    -- Or set it to 0 to have better visuals.
-                    throttle = 20,
-
-                    -- The minimum length of the message, otherwise it will be on a new line.
-                    softwrap = 15,
-
-                    -- If multiple diagnostics are under the cursor, display all of them.
-                    multiple_diag_under_cursor = false,
-
-                    -- Enable diagnostic message on all lines.
-                    -- /!\ Still an experimental feature, can be slow on big files.
-                    multilines = false,
-
-                    overflow = {
-                        -- Manage the overflow of the message.
-                        --    - wrap: when the message is too long, it is then displayed on multiple lines.
-                        --    - none: the message will not be truncated, and will be displayed on a single line.
-                        mode = "wrap",
-                    },
-
-                    --- Enable it if you want to always have message with `after` characters length.
-                    break_line = {
-                        enabled = false,
-                        after = 30,
-                    },
-
-                    virt_texts = {
-                        priority = 2048,
-                    },
-                },
-            })
-        end
-    }
 }
